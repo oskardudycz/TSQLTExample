18b15ceca69b8c83a64826cbe174ccecfbee23bd
oskar.dudycz@gmail.com <styczen2STYCZEN@>
2014-11-18 17:08:32 +0100

    Initial commit

---
+++ UserManagementPortal/Objects/User/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [UserManagement].[Users]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Login] VARCHAR(255),
+    [City] VARCHAR(255)
+)
---
+++ UserManagementPortal/Procedures/User/AddUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [UserManagement].[AddUser]
+    @UserName VARCHAR (255)
+AS
+
+    -- INSERT INTO [UserManagement].Users (Id, Login) VALUES (1,2)
+    -- INSERT INTO [UserManagement].Users (Login) VALUES (1,2)
+    INSERT INTO [UserManagement].Users (Login) VALUES (@UserName)    
+RETURN 
+    SCOPE_IDENTITY();
