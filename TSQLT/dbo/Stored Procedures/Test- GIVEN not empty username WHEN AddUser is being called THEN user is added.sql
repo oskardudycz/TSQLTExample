05de8b73d9467e5b7bef8dd9250fe377fe0e07e0
oskar.dudycz@gmail.com <styczen2STYCZEN@>
2014-11-18 19:33:15 +0100

    tests added

---
+++ TSQLT/dbo/Stored Procedures/Test- GIVEN not empty username WHEN AddUser is being called THEN user is added.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[Test- GIVEN not empty username WHEN AddUser is being called THEN user is added]
+    @param1 int = 0,
+    @param2 int
+AS
+    SELECT @param1, @param2
+RETURN 0
